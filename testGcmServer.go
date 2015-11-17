ad898a51fb0b243888e4c75206ef0ebd2ec01c7c
junglesung <junglesung@gmail.com>
2015-11-17 22:53:50 +0800

    Implement UpdateUser(). TODO: Test it

--- testGcmServer.go
+++ testGcmServer.go
@@ -10,22 +10,32 @@ import (
 	"strings"
 	"fmt"
 	"appengine/urlfetch"
+	"errors"
 )
 
-type Member struct {
-	Id string `json:"id"`
-	Token string `json:"token"`
-	Message string `json:"message"`
-	CreateTime time.Time `json:"createtime"`
+// HTTP body of user registration or token update
+// Datastore User Kind
+type UserRegistration struct {
+	RegistrationToken    string    `json:"registrationtoken      datastore:"-"`
+	InstanceId           string    `json:"instanceid`
+	NewRegistrationToken string    `json:"newregistrationtoken   datastore:"RegistrationToken"`
+	LastUpdateTime       time.Time `json:"lastupdatetime`
+}
+
+// User unregistration
+type UserUnregistration struct {
+	RegistrationToken    string    `json:"registrationtoken      datastore:"-"`
+	InstanceId           string    `json:"instanceid`
 }
 
 type HelloMessage struct {
-	Message string `json:"message"`
+	RegistrationToken    string    `json:"registrationtoken      datastore:"-"`
+	Message              string    `json:"message"`
 }
 
 const BaseUrl = "/api/0.1/"
-const MemberKind = "Member"
-const MemberRoot = "Member root"
+const UserKind = "User"
+const UserRoot = "User root"
 
 // GCM server
 const GcmURL = "https://gcm-http.googleapis.com/gcm/send"
@@ -33,7 +43,7 @@ const GcmApiKey = "AIzaSyAODu6tKbQp8sAwEBDNLzW9uDCBmmluQ4A"
 
 func init() {
 	http.HandleFunc(BaseUrl, rootPage)
-	http.HandleFunc(BaseUrl+"members", members)
+	http.HandleFunc(BaseUrl+"users", users)
 	http.HandleFunc(BaseUrl+"tokens/", EchoMessage)
 }
 
@@ -42,18 +52,19 @@ func rootPage(rw http.ResponseWriter, req *http.Request) {
 	c.Debugf("This is root")
 }
 
-func members(rw http.ResponseWriter, req *http.Request) {
+func users(rw http.ResponseWriter, req *http.Request) {
 	switch req.Method {
-	case "GET":
-		listMember(rw, req)
-	case "POST":
-		addMember(rw, req)
-//	case "PUT":
-//		updateMember(rw, req)
-	case "DELETE":
-		clearMember(rw, req)
+//	case "GET":
+//		listMember(rw, req)
+	// To avoid duplicate users, use PUT to search the existing one before adding a new one
+//	case "POST":
+//		addUser(rw, req)
+	case "PUT":
+		UpdateUser(rw, req)
+//	case "DELETE":
+//		clearMember(rw, req)
 	default:
-		listMember(rw, req)
+		http.Error(rw, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 	}
 }
 
@@ -164,13 +175,12 @@ func EchoMessage(rw http.ResponseWriter, req *http.Request) {
 	c.Infof("%s", respBody)
 }
 
-// Register a client and reply "Hello~"
-func addMember(rw http.ResponseWriter, req *http.Request) {
+func UpdateUser(rw http.ResponseWriter, req *http.Request) {
 	// Appengine
 	var c appengine.Context = appengine.NewContext(req)
 	// Result, 0: success, 1: failed
 	var r int = 0
-	var cKey *datastore.Key = nil
+	var cKey, pKey *datastore.Key = nil, nil
 	defer func() {
 		// Return status. WriteHeader() must be called before call to Write
 		if r == 0 {
@@ -189,27 +199,71 @@ func addMember(rw http.ResponseWriter, req *http.Request) {
 		r = 1
 		return
 	}
-	var member Member
-	if err = json.Unmarshal(b, &member); err != nil {
+
+	// Vernon debug
+	c.Debugf("Got body %s", b)
+
+	var user UserRegistration
+	if err = json.Unmarshal(b, &user); err != nil {
 		c.Errorf("%s in decoding body %s", err, b)
 		r = 1
 		return
 	}
 
 	// Set now as the creation time. Precision to a second.
-	member.CreateTime = time.Unix(time.Now().Unix(), 0)
+	user.LastUpdateTime = time.Unix(time.Now().Unix(), 0)
 
-	// Vernon debug
-	c.Debugf("Store member %s", b)
-
-	// Store item into datastore
-	pKey := datastore.NewKey(c, MemberKind, MemberRoot, 0, nil)
-	cKey, err = datastore.Put(c, datastore.NewIncompleteKey(c, MemberKind, pKey), &member)
+	// Search for existing user
+	pKey, err = searchUser(user, c)
 	if err != nil {
-		c.Errorf("%s in storing to datastore", err)
+		c.Errorf("%s in searching existing user %v", err, user)
 		r = 1
 		return
 	}
+	if pKey == nil {
+		// Add new user into datastore
+		pKey = datastore.NewKey(c, UserKind, UserRoot, 0, nil)
+		cKey, err = datastore.Put(c, datastore.NewIncompleteKey(c, UserKind, pKey), &user)
+		if err != nil {
+			c.Errorf("%s in storing to datastore", err)
+			r = 1
+			return
+		}
+		c.Debugf("Add user %v", user)
+	} else {
+		// Update existing user in datastore
+		cKey, err = datastore.Put(c, pKey, &user)
+		if err != nil {
+			c.Errorf("%s in storing to datastore", err)
+			r = 1
+			return
+		}
+		c.Debugf("Update user %v", user)
+	}
+
+}
+
+func searchUser(user UserRegistration, c appengine.Context) (key *datastore.Key, err error) {
+	// Initial variables
+	key = nil
+	err = nil
+
+	// Query
+	f := datastore.NewQuery(UserKind)
+	f = f.Filter("InstanceId=", user.InstanceId).KeysOnly()
+	k, err := f.GetAll(c, nil)
+	if err != nil {
+		c.Errorf("%s in getting data from datastore\n", err)
+		err = errors.New("Datastore is temporary unavailable")
+		return
+	}
+
+	if k == nil || len(k) == 0 {
+		return
+	}
+
+	key = k[0]
+	return
 }
 
 //func queryMember(rw http.ResponseWriter, req *http.Request) {
@@ -242,65 +296,65 @@ func addMember(rw http.ResponseWriter, req *http.Request) {
 //	}
 //}
 
-func listMember(rw http.ResponseWriter, req *http.Request) {
-	// To access datastore and to log
-	c := appengine.NewContext(req)
-	c.Debugf("listMember()")
-
-	// Get all entities
-	var dst []Member
-	r := 0
-	k, err := datastore.NewQuery(MemberKind).Order("-CreateTime").GetAll(c, &dst)
-	if err != nil {
-		c.Errorf("%s", err)
-		r = 1
-	}
-
-	// Map keys and items
-	for i, v := range k {
-		dst[i].Id = v.Encode()
-	}
-
-	// Return status. WriteHeader() must be called before call to Write
-	if r == 0 {
-		rw.WriteHeader(http.StatusOK)
-	} else {
-		http.Error(rw, http.StatusText(http.StatusNotFound), http.StatusNotFound)
-		return
-	}
-
-	// Return body
-	encoder := json.NewEncoder(rw)
-	if err = encoder.Encode(dst); err != nil {
-		c.Errorf("%s in encoding result %v", err, dst)
-	} else {
-		c.Infof("listMember() returns %d members", len(dst))
-	}
-}
-
-func clearMember(rw http.ResponseWriter, req *http.Request) {
-	// To access datastore and to log
-	c := appengine.NewContext(req)
-	c.Infof("clearMember()")
-
-	// Delete root entity after other entities
-	r := 0
-	pKey := datastore.NewKey(c, MemberKind, MemberRoot, 0, nil)
-	if keys, err := datastore.NewQuery(MemberKind).KeysOnly().GetAll(c, nil); err != nil {
-		c.Errorf("%s", err)
-		r = 1
-	} else if err := datastore.DeleteMulti(c, keys); err != nil {
-		c.Errorf("%s", err)
-		r = 1
-	} else if err := datastore.Delete(c, pKey); err != nil {
-		c.Errorf("%s", err)
-		r = 1
-	}
-
-	// Return status. WriteHeader() must be called before call to Write
-	if r == 0 {
-		rw.WriteHeader(http.StatusOK)
-	} else {
-		http.Error(rw, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-	}
-}
+//func listMember(rw http.ResponseWriter, req *http.Request) {
+//	// To access datastore and to log
+//	c := appengine.NewContext(req)
+//	c.Debugf("listMember()")
+//
+//	// Get all entities
+//	var dst []UserRegistration
+//	r := 0
+//	k, err := datastore.NewQuery(UserKind).Order("-CreateTime").GetAll(c, &dst)
+//	if err != nil {
+//		c.Errorf("%s", err)
+//		r = 1
+//	}
+//
+//	// Map keys and items
+//	for i, v := range k {
+//		dst[i].Id = v.Encode()
+//	}
+//
+//	// Return status. WriteHeader() must be called before call to Write
+//	if r == 0 {
+//		rw.WriteHeader(http.StatusOK)
+//	} else {
+//		http.Error(rw, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+//		return
+//	}
+//
+//	// Return body
+//	encoder := json.NewEncoder(rw)
+//	if err = encoder.Encode(dst); err != nil {
+//		c.Errorf("%s in encoding result %v", err, dst)
+//	} else {
+//		c.Infof("listMember() returns %d members", len(dst))
+//	}
+//}
+//
+//func clearMember(rw http.ResponseWriter, req *http.Request) {
+//	// To access datastore and to log
+//	c := appengine.NewContext(req)
+//	c.Infof("clearMember()")
+//
+//	// Delete root entity after other entities
+//	r := 0
+//	pKey := datastore.NewKey(c, UserKind, UserRoot, 0, nil)
+//	if keys, err := datastore.NewQuery(UserKind).KeysOnly().GetAll(c, nil); err != nil {
+//		c.Errorf("%s", err)
+//		r = 1
+//	} else if err := datastore.DeleteMulti(c, keys); err != nil {
+//		c.Errorf("%s", err)
+//		r = 1
+//	} else if err := datastore.Delete(c, pKey); err != nil {
+//		c.Errorf("%s", err)
+//		r = 1
+//	}
+//
+//	// Return status. WriteHeader() must be called before call to Write
+//	if r == 0 {
+//		rw.WriteHeader(http.StatusOK)
+//	} else {
+//		http.Error(rw, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+//	}
+//}
